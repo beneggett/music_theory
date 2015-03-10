aeb3de1c454432ba0419b7895992dbce11ef7e08
Ben Eggett <beneggett@gmail.com>
2015-03-10 03:22:29 -0600

    Removes poorly pasted code from transpose (placeholder)

--- lib/music_theory/transpose.rb
+++ lib/music_theory/transpose.rb
@@ -6,21 +6,9 @@ module MusicTheory
 
     attr_accessor :samples
 
-    def initialize(*things_to_flatten)
-      @samples = []
-      [*things_to_flatten].each do |group|
-        group.each_with_index do |value, i|
-         @samples[i] ||= 0
-         @samples[i] +=  value
-        end
-      end
-       max = @samples.map {|s| s.abs }.max
-       multiplier = 1.0 / max
-       @samples.map!{ |s| multiplier * s }
+    def initialize
+      # Add code that transposes up or down x octaves
     end
-
-
-
 
   end
 end
--- lib/music_theory/version.rb
+++ lib/music_theory/version.rb
@@ -1,3 +1,3 @@
 module MusicTheory
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
